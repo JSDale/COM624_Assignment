2992dc5b13406e35fedd51782a5302714483287f
jsdale <jdale994@googlemail.com>
2021-10-21 12:28:18 +0100

    added more code clean up

--- PandasStockPredictor.py
+++ PandasStockPredictor.py
@@ -8,26 +8,6 @@ import PlotReturnsGraph
 filepath = os.getcwd()
 
 
-def get_closing_stock_price(start_date, end_date, ticker):
-    
-    df = web.DataReader(ticker, 'yahoo', start_date, end_date)
-    df.to_json(filepath+'/Data/raw_data.json')
-    df.tail()
-    close_px = df['Adj Close']
-    return close_px
-
-
-def get_moving_average(closing_stock_price):
-    rolling_mean = closing_stock_price.rolling(window=100).mean()
-    rolling_mean.to_json(filepath + '/Data/rolling_data.json')
-    return rolling_mean
-
-
-def calculate_return_value(closing_stock_price):
-    returns_value = closing_stock_price / closing_stock_price.shift(1) - 1
-    return returns_value
-
-
 def render_rolling_average_to_graph():
     try:
         ticker = 'AAPL'
@@ -41,3 +21,27 @@ def render_rolling_average_to_graph():
         print('graphs plotted')
     except Exception as e:
         print(f"error: {e.message}")
+
+
+def get_closing_stock_price(start_date, end_date, ticker):
+    
+    df = web.DataReader(ticker, 'yahoo', start_date, end_date)
+    save_to_json(df, 'raw_data.json')
+    df.tail()
+    close_px = df['Adj Close']
+    return close_px
+
+
+def save_to_json(raw_dataframe, filename):
+    raw_dataframe.to_json(f'{filepath}/Data/{filename}.json')
+
+
+def get_moving_average(closing_stock_price):
+    rolling_mean = closing_stock_price.rolling(window=100).mean()
+    save_to_json(rolling_mean, 'rolling_data')
+    return rolling_mean
+
+
+def calculate_return_value(closing_stock_price):
+    returns_value = closing_stock_price / closing_stock_price.shift(1) - 1
+    return returns_value
